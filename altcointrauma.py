3e47b68cb0702aa8704c89dc301246e4b960b531
Bryson Shea <bryson.w.shea@gmail.com>
2022-05-10 22:36:04 -1000

    functionized it, add sleep function

--- altcointrauma.py
+++ altcointrauma.py
@@ -1,13 +1,36 @@
+from locale import getpreferredencoding
+from pickle import TRUE
+from time import time
 from urllib import request
+import time
 import requests
 
-price_url = 'https://api.coingecko.com/api/v3/coins/zookeeper?localization=false&tickers=false&market_data=true'
-response = requests.get(price_url)
-current_price = response.json()['market_data']['current_price']['usd']
+def getprice_postifttt():
 
-url = 'https://maker.ifttt.com/trigger/test_event/with/key/cu-0QxWSdbV5uQRZTdr8Ol'
-data = {
-    "value1":current_price
-    }
-requests.post(url, json=data)
+    while TRUE:
+        price_url = 'https://api.coingecko.com/api/v3/coins/zookeeper?localization=false&tickers=false&market_data=true'
+        response = requests.get(price_url)
+
+        #currency prices
+        current_usd = response.json()['market_data']['current_price']['usd']
+        current_rub = response.json()['market_data']['current_price']['rub']
+
+        ifttt_url = 'https://maker.ifttt.com/trigger/test_event/with/key/cu-0QxWSdbV5uQRZTdr8Ol'
+        data = {
+            "value1":current_usd,
+            "value2":current_rub
+            }
+        requests.post(ifttt_url, json=data)
+        time.sleep(21600)
+
+getprice_postifttt()
+
+
+
+
+
+
+
+
+
 
